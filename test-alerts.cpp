c03bd2fec150b82bbe0147c35de3eb5fdfc8762a
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-01-24 20:07:55 +0530

    Inital commit

---
+++ test-alerts.cpp
@@ -0,0 +1,9 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "test/catch.hpp"
+#include "RangeCheck.h"
+
+TEST_CASE("Range readings checking") {
+  short arrData[] = {4,5};
+  REQUIRE(drivenRangeCheck(arrData) == 2);
+}
