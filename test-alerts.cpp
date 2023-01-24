265ae17c74c60280005cc2d800d0276491b39cd4
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-01-24 21:22:01 +0530

    test case update

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -12,3 +12,8 @@ TEST_CASE("Range readings checking with 4 array of data with misalign array data
   short arrData[] = {3,5,4};
   REQUIRE(drivenRangeCheck(arrData,3) == 3);
 }
+
+TEST_CASE("Range readings checking with 5 array of data with Same value stored in array") {
+  short arrData[] = {3,5,4,3};
+  REQUIRE(drivenRangeCheck(arrData,4) == 4);
+}
