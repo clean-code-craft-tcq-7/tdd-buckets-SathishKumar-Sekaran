f691885eae2a6b14babb23e9efa04ed7f7c4eb60
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-02-08 10:47:58 +0530

    Function updated

--- AdcConverter.c
+++ AdcConverter.c
@@ -1,17 +1,41 @@
 #include <stdio.h>
 #include "AdcConverter.h"
 
-void adcConverterReadValue(int minRange,int maxRange,int adcBitRate,int* DataCount,int DataLen,int* outputAmps)
-{
-    
-}
-
-int roundOfVal(float floatVal);
-{
-
-}
-
 int PowerOf2(int bitRate)
 {
-    
+    int returnVal = 1;
+    while(bitRate > 0)
+    {
+        returnVal *=2;
+        bitRate--;
+    }
+    return returnVal;
+}
+
+int roundOfVal(float floatVal)
+{
+    int Intval = (int)floatVal;
+    float roundVal = floatVal - (float)Intval;
+    if(roundVal > 0.5)
+    {
+        return (int)(floatVal+0.5);
+    }
+    else
+    {
+        return Intval;
+    }
+}
+
+void adcConverterReadValue(int minRange,int maxRange,int adcBitRate,int* DataCount,int DataLen,int* outputAmps)
+{
+    int Max_differentVal_Amps = maxRange - minRange;
+    float out[DataLen];
+    int loopcnt = 0;
+    while(loopcnt<DataLen)
+    {
+        out[loopcnt] = (float)Max_differentVal_Amps *(float)((float)DataCount[loopcnt]/(float)PowerOf2(adcBitRate));
+        out[loopcnt] += minRange;
+        outputAmps[loopcnt] = roundOfVal(out[loopcnt]);
+        loopcnt++;
+    }
 }
