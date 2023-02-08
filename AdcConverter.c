cbb051bfd9a6e408c0cd3f64f96043f0b48273a4
Sathish <Sekaran.SathishKumar@in.bosch.com>
2023-02-08 10:44:17 +0530

    ADC converter Initial commit

---
+++ AdcConverter.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "AdcConverter.h"
+
+void adcConverterReadValue(int minRange,int maxRange,int adcBitRate,int* DataCount,int DataLen,int* outputAmps)
+{
+    
+}
+
+int roundOfVal(float floatVal);
+{
+
+}
+
+int PowerOf2(int bitRate)
+{
+    
+}
---
+++ AdcConverter.h
@@ -0,0 +1,3 @@
+void adcConverterReadValue(int minRange,int maxRange,int adcBitRate,int* DataCount,int DataLen,int* outputAmps);
+int roundOfVal(float floatVal);
+int PowerOf2(int bitRate);
